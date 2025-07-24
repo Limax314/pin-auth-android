1446951f5d688662bd794174928d2eb596de9ccb
sunwook <sw98@hwajintni.co.kr>
2025-07-24 10:51:03 +0900

    refactor(app): 루트 프로젝트명 변경
    - 저장소 이름과 같은 값으로 변경
    - 코드 정렬

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,6 +14,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "PinTextField-Usage"
+rootProject.name = "pin-auth-android"
+
 include(":app")
 include(":PinTextFieldLib")
