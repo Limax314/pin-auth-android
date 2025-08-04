dd63205b767328600476a8c1ab485ed861a900ac
sunwook <sw98@hwajintni.co.kr>
2025-08-04 09:46:50 +0900

    refactor(Theme.kt): KDoc 주석 추가
    - 파일 설명, 함수 설명 추가

--- app/src/main/java/com/sharpedge/pintextfieldusage/ui/theme/Theme.kt
+++ app/src/main/java/com/sharpedge/pintextfieldusage/ui/theme/Theme.kt
@@ -16,16 +16,13 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 /**
- * @remarks
- * KDoc 주석 추가 예정
- */
-
-/**
- * @remarks
- * pin-auth-android 프로젝트 배포 예정
+ * @file Theme.kt
+ * @brief 애플리케이션의 테마(색상 구성표, 글꼴 스타일 등)를 정의합니다.
  *
- * [계획]
- * - [README.md] 내 `실제 기기에 앱 배포 및 테스트하기` 참고
+ * 이 파일은 PinTextFieldUsage 애플리케이션의 [MaterialTheme]을 설정합니다.
+ * 여기에는 다크 및 라이트 색상 팔레트에 대한 정의와 시스템 설정에 따라 올바른 테마를 적용하는
+ * [PinTextFieldUsageTheme] 컴포저블 함수가 포함되어 있습니다.
+ * 또한 Android 12 이상에서 동적 색상 지정을 지원합니다.
  */
 
 private val DarkColorScheme = darkColorScheme(
@@ -50,6 +47,18 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+/**
+ * 주어진 콘텐츠에 애플리케이션의 테마를 적용하는 컴포저블 함수입니다.
+ *
+ * 이 함수는 적절한 색상 구성표(라이트/다크, 동적/정적)를 결정하고
+ * [MaterialTheme]을 사용하여 적용합니다.
+ * 또한 시스템 상태 표시줄 색상을 관리합니다.
+ *
+ * @param darkTheme 테마 다크 모드 설정 여부, 기본값: 시스템 설정
+ * @param dynamicColor 동적 색상(Material You) 사용 여부, Android 12 이상에서 사용 가능,
+ * 기본값: true
+ * @param content 테마를 적용할 컴포저블 콘텐츠
+ */
 @Composable
 fun PinTextFieldUsageTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
